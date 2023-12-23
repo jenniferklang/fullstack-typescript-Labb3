0ef8afcbbc9a796323358d81abc27a8d77c52be4
Jennifer Klang <klang.jennifer@gmail.com>
2023-12-23 13:34:16 +0100

    Add tests for the new scenarios

--- frontend/cypress/e2e/Log.ts
+++ frontend/cypress/e2e/Log.ts
@@ -16,6 +16,34 @@ Then("Dagens entry är tillagd och formuläret blir rensat", () => {
   cy.get(".log-list-container").should("contain.text", "Anteckning-test");
 });
 
+// Markera vald anteckning för att göra ändring
+Given(
+  "Kalender med eventuella tillagda anteckningar visas, representerade som små prickar",
+  () => {}
+);
+
+When("Jag klickar på en befintlig anteckning för valt datum", () => {
+  cy.get(".log-entry").first().click();
+});
+
+Then("Anteckningen får en färgad ram runt sig, den är markerad", () => {
+  cy.get(".log-entry.selected").should("exist");
+});
+
+// Ta bort vald anteckning
+Given(
+  "Tillagda anteckningar visas och vald anteckning har en färgad ram runt sig",
+  () => {}
+);
+
+When("Jag klickar på knappen Ta bort", () => {
+  cy.get("#delete-button").click();
+});
+
+Then("Vald anteckning är borttagen", () => {
+  cy.get(".log-list-container").should("not.contain.text", "Anteckning-test");
+});
+
 // import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 // Given(
