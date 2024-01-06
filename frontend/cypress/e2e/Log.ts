cb574f320a4cb2ad071e6b1ec8fbc3f8ea13ab97
Jennifer Klang <klang.jennifer@gmail.com>
2024-01-06 14:43:53 +0100

    Add more DRY-thinking to my ts-file with given,when,then etc

--- frontend/cypress/e2e/Log.ts
+++ frontend/cypress/e2e/Log.ts
@@ -40,13 +40,18 @@ Then("Anteckningen får en färgad ram runt sig, den är markerad", () => {
 
 Given("Tillagda anteckningar visas för valt datum", () => {});
 
+// Ett försök till att göra koden mer DRY genom att skapa en gemensam funktion
+const selectPushButton = (buttonText: "Ta bort" | "Ändra") => {
+  cy.get(".log-entry").first().as("selectedEntry");
+  cy.get("@selectedEntry").invoke("attr", "data-id").as("selectedEntryId");
+  cy.get("@selectedEntry").click();
+  cy.contains(buttonText).should("be.visible").click();
+};
+
 When(
-  "Jag markerar en befintlig anteckning och klickar på ta bort-knapp som visas",
-  () => {
-    cy.get(".log-entry").first().as("selectedEntry");
-    cy.get("@selectedEntry").invoke("attr", "data-id").as("selectedEntryId");
-    cy.get("@selectedEntry").click();
-    cy.contains("Ta bort").should("be.visible").click();
+  'Jag markerar en befintlig anteckning och klickar på "{string}"-knapp som visas',
+  (buttonText: "Ta bort" | "Ändra") => {
+    selectPushButton(buttonText);
   }
 );
 
@@ -55,3 +60,44 @@ Then("Vald anteckning är borttagen", () => {
     cy.get(`.log-entry[data-id="${selectedId}"]`).should("not.exist");
   });
 });
+
+When(
+  'Jag markerar en befintlig anteckning och klickar på "{string}"-knapp som visas',
+  (buttonText: "Ta bort" | "Ändra") => {
+    selectPushButton(buttonText);
+  }
+);
+
+Then("All information vid vald anteckning visas och kan ändras", () => {
+  cy.get("#content").should("exist");
+  cy.get("#symptoms").should("exist");
+  cy.get("#meal").should("exist");
+
+  cy.get("@selectedEntry").then(($selectedEntry) => {
+    const content = $selectedEntry.find(".content").text();
+    const symptoms = $selectedEntry.find(".symptoms").text();
+    const meal = $selectedEntry.find(".meal").text();
+
+    cy.get("#content").clear().type(content);
+    cy.get("#symptoms").clear().type(symptoms);
+    cy.get("#meal").clear().type(meal);
+  });
+
+  cy.contains("Spara ändringar").should("be.visible").click();
+});
+
+// When(
+//   "Jag markerar en befintlig anteckning och klickar på ta bort-knapp som visas",
+//   () => {
+//     cy.get(".log-entry").first().as("selectedEntry");
+//     cy.get("@selectedEntry").invoke("attr", "data-id").as("selectedEntryId");
+//     cy.get("@selectedEntry").click();
+//     cy.contains("Ta bort").should("be.visible").click();
+//   }
+// );
+
+// Then("Vald anteckning är borttagen", () => {
+//   cy.get("@selectedEntryId").then((selectedId) => {
+//     cy.get(`.log-entry[data-id="${selectedId}"]`).should("not.exist");
+//   });
+// });
