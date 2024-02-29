909b1d21ad5e9389bc462bd0921a3d34a839c379
Jennifer Klang <klang.jennifer@gmail.com>
2024-02-29 22:27:32 +0100

    Add some media queryy

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,17 +6,8 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <span className="navbar-toggler-icon"></span>
+
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
